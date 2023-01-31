04aec6231cc241528efd09fec8e60498d3d7e9b0
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2023-01-30 23:59:11 -0500

    fix-test-case

--- index.test.js
+++ index.test.js
@@ -1,30 +1,25 @@
 import request from "supertest";
 import app from "./server.js";
 
-describe("GET /", function () {
+describe("Check APIs", function () {
+  const mockApp = request(app);
   test("Check if API is working", async function () {
-    const response = await request(app).get("/healthz");
+    const response = await mockApp.get("/healthz");
     expect(response.status).toEqual(200);
   });
-});
 
-describe("GET user API /", function () {
   test("Check if GET user API is working", async function () {
-    const response = await request(app).get("/v1/user/1");
+    const response = await mockApp.get("/v1/user/1");
+    expect(response.status).toEqual(403);
+  });
+
+  test("Check if POST user API sending bad request", async function () {
+    const response = await mockApp.post("/v1/user");
+    expect(response.status).toEqual(400);
+  });
+
+  test("Check if POST user API sending bad request", async function () {
+    const response = await mockApp.put("/v1/user/1");
     expect(response.status).toEqual(403);
   });
 });
-
-describe("POST user API /", function () {
-  test("Check if POST user API sending bad request", async function () {
-    const response = await request(app).post("/v1/user");
-    expect(response.status).toEqual(400);
-  });
-});
-
-// describe("UPDATE user API /", function () {
-//   test("Check if POST user API sending bad request", async function () {
-//     const response = await request(app).put("/v1/user/1");
-//     expect(response.status).toEqual(403);
-//   });
-// });
